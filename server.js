559dc4924ab13ef88c7adf2accb3de5713f99188
Praveen9963-77 <avukpraveen779@gmail.com>
2025-11-06 10:24:48 +0530

    server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.post("/api/predict", upload.single("image"), async (req, res) => {
     formData.append("image", fs.createReadStream(filePath));
 
     // send image to Flask backend
-    const response = await fetch("http://127.0.0.1:5000/predict", {
+    const response = await fetch("https://breedprediction-3.onrender.com/predict", {
       method: "POST",
       body: formData,
       headers: formData.getHeaders(), // ⬅️ required when using form-data
