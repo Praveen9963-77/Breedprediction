cea49295d429e648f7b60c579997de1089936355
Praveen9963-77 <avukpraveen779@gmail.com>
2025-11-07 13:15:26 +0530

    shduhsd

--- predict.py
+++ predict.py
@@ -11,7 +11,6 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     # Placeholder for your actual logic
-    print('bhghjuhjkkjk')
     predicted_class = "Golden Retriever"
     confidence = 0.97
 
